5dfd42a676bc9b78997906162fee2db8bfbf9a5c
pinguo-marui <marui@camera360.com>
2014-07-16 10:31:13 +0800

    add log util

--- app/src/main/java/com/okry/okframework/base/BaseActivity.java
+++ app/src/main/java/com/okry/okframework/base/BaseActivity.java
@@ -1,7 +1,50 @@
 package com.okry.okframework.base;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.okry.okframework.log.L;
+
 /**
  * Created by mr on 14-7-16.
  */
-public class BaseActivity {
+public class BaseActivity extends FragmentActivity{
+    protected final String LIFE_CYCLE_TAG = "lifecycle" + "[%s]";
+    protected final String CLASS_TAG = getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        L.d(LIFE_CYCLE_TAG, CLASS_TAG + ":onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        L.d(LIFE_CYCLE_TAG, CLASS_TAG + ":onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        L.d(LIFE_CYCLE_TAG, CLASS_TAG + ":onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        L.d(LIFE_CYCLE_TAG, CLASS_TAG + ":onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        L.d(LIFE_CYCLE_TAG, CLASS_TAG + ":onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        L.d(LIFE_CYCLE_TAG, CLASS_TAG + ":onDestroy");
+    }
 }
